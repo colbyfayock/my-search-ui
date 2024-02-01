abc613cf3c74d0cd761a63457e3613cf71e1c2d1
Colby Fayock <fayock@gmail.com>
2024-01-31 22:13:13 -0500

    dismissing search dropdown ui on escape and click outside search form

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -8,12 +8,43 @@ const Search = () => {
   const [posts, setPosts] = useState();
   const { query, results, search, reset } = useSearch({ posts, maxResults: 5 });
   
+  const hasResults = results && results.length > 0;
+
   useEffect(() => {
     (async function run() {
       const { posts: data } = await fetch('/posts.json').then(r => r.json());
       setPosts(data);
     })()
   }, []);
+
+  function handleOnClick(event: MouseEvent) {
+    // Look for the form in the path of elements starting at
+    // the element that was clicked and look for the form.
+    // If the form is found, that means someone's clicking
+    // inside the search area
+    if ( formRef.current && !event.composedPath().includes(formRef.current) ) {
+      reset();
+    }
+  }
+
+  function handleOnKeyDown(event: KeyboardEvent) {
+    // If someone hits the escape key, we want to reset!
+    if ( event.keyCode === 27 || event.code === 'Escape' ) {
+      reset();
+    }
+  }
+
+  useEffect(() => {
+    if ( !hasResults ) return;
+    document.body.addEventListener('click', handleOnClick);
+    document.body.addEventListener('keydown', handleOnKeyDown);
+    return () => {
+      // Be sure to clean up event listeners when the
+      // component unmounts
+      document.body.removeEventListener('click', handleOnClick);
+      document.body.removeEventListener('keydown', handleOnKeyDown);
+    }
+  }, [hasResults])
 
   return (
     <form ref={formRef} action="/search" className="relative">
@@ -22,7 +53,7 @@ const Search = () => {
       </label>
       <input
         id="search-query"
-        type="text"
+        type="search"
         name="q"
         value={query || ''}
         onChange={(event) => search(event.currentTarget.value)}
